f713704424ac6dd4304d3f3b52de20155a1d6a4e
FABIANO SODER <fabianosoder@hotmail.com>
2023-04-22 10:01:00 -0300

    added sleep to wait 2sec before executing task

--- tasks.py
+++ tasks.py
@@ -1,9 +1,11 @@
 # tasks.py
 from celery import Celery
+import time
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
 @app.task
 def add(x, y):
+    time.sleep(2)
     print("Tarefa em execução!")
     return x + y
